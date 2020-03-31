c62fdc5f497f928e8c55881b6064359ae4ac0e8d
Mikheull <mikhael.bailly1@orange.fr>
2020-04-01 00:06:13 +0200

    add the Soon page and images

--- routes/routes.js
+++ routes/routes.js
@@ -1,14 +1,14 @@
 const express = require('express');
 const router = express.Router();
 
-router.use('/', require('./index') );
-router.use('/api', require('./api') );
-router.use('/customize', require('./customize') );
-router.use('/s', require('./s') );
-router.use('/embed', require('./embed') );
+// router.use('/', require('./index') );
+// router.use('/api', require('./api') );
+// router.use('/customize', require('./customize') );
+// router.use('/s', require('./s') );
+// router.use('/embed', require('./embed') );
 
 router.use(function(req,res){
-    res.status(404).send('404');
+    res.render('soon.ejs');
 });
 
 module.exports = router;
